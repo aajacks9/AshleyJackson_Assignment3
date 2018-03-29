28dcfedfa950d6c3e03c7709b60fa480569ab577
aajacks9 <aajacks@gmail.com>
2018-03-29 18:40:38 -0500

    pages properly linked
    edits on the index.js page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+    res.render('index', {
+        title: 'Home'
+    });
+});
+
+router.get('/Login', function(req, res){
+    res.render('Login', {
+        title: 'Login'
+    });
+});
+
+router.get('/notes', function(req, res){
+    res.render('notes', {
+        title: 'notes'
+    });
+});
+
+router.get('/editor', function(req, res){
+    res.render('editor', {
+        title: 'Edit Notes'
+    });
 });
 
 module.exports = router;
